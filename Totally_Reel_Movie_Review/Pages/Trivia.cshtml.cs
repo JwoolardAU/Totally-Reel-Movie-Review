059727cbc92f2506912ef1f8885461d45ed99646
assellak <assellak@augusta.edu>
2020-10-02 21:16:03 -0400

    added new pages

---
+++ Totally_Reel_Movie_Review/Pages/Trivia.cshtml.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Totally_Reel_Movie_Review.Pages
+{
+}
