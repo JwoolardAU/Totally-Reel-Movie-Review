338ad2bc1475294901da52162efbdf53db3f9109
Patrick <patrickwoolard48@gmail.com>
2020-11-08 13:17:36 -0500

    Noticed my MovieTemplate Page wasn't in the repo. This is to add it

---
+++ TRMR/Pages/MovieTemplate.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using TRMR.Models;
+using TRMR.Data;
+
+namespace TRMR.Pages
+{
+    public class MovieTemplateModel : PageModel
+    {
+
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _dbContext;
+
+        public MovieTemplateModel(ILogger<IndexModel> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public Movie CurrentMovie{get; set;} 
+        public IEnumerable<Review> Reviews {get; set;} = new List<Review>();
+
+        public IActionResult OnGet(int? id) //ARGUMENT NAME HAS TO MATCH QUERY STRING METADATA NAME (I.E. "id = blah")
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            CurrentMovie = _dbContext.Movies.Find(id);
+
+            Reviews = _dbContext.Review.Where(s => s.Movie_Id == id);
+
+            if(CurrentMovie  == null)
+            {
+                return NotFound();
+            }
+
+            return Page();        
+        }
+    }
+}
