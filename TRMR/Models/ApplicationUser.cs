771f142529eb6543c46a5cfb0999b6afd98b2971
JwoolardAU <jwoolard@augusta.edu>
2020-11-28 10:31:20 -0500

    added applicationuser

---
+++ TRMR/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace TRMR.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //[Required] // may need to be added with a new migration&update
+        public string ReelName {get; set;}  
+    }
+}
