d50ed2049891006bbaf6d18ee0b0be14a9100fb2
Paul York <paul@yorkfamily.com>
2020-11-28 12:18:27 -0500

    added scaffolded identity area

---
+++ TRMR/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TRMR.Data;
+using TRMR.Models;
+
+[assembly: HostingStartup(typeof(TRMR.Areas.Identity.IdentityHostingStartup))]
+namespace TRMR.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
