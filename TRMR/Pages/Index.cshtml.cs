8426bc180c404d71f6ee09ee0f9e6906124639b5
Mark Holcomb <markholcomb5@gmail.com>
2020-12-03 15:47:55 -0500

    Merged together Assya and my work

--- TRMR/Pages/Index.cshtml.cs
+++ TRMR/Pages/Index.cshtml.cs
@@ -28,11 +28,13 @@ namespace TRMR.Pages
         }
 
         public List<Movie> Movies = new List<Movie>(); 
+        public List<Review> Reviews {get; set;} = new List<Review>();
         
 
         public async void OnGet()
         {
             this.Movies.AddRange(_dbContext.Movies);
+            this.Reviews.AddRange(_dbContext.Review);
         }
     }
 }
